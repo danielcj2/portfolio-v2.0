240546c5c47aa0c31157c66b2cb41c15d4a6be34
Daniel Cojocea <danielcojocea28@gmail.com>
2026-05-05 11:54:07 -0400

    added initial config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,57 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.ts",
+      },
+      "*.{webm|mp4}": {
+        as: "*.js",
+        loaders: [],
+      },
+    },
+  },
+  webpack(config) {
+    config.module.rules.push(
+      {
+        test: /\.(webm|mp4)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.svg$/i,
+        type: "asset",
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: [/url/] }, // exclude react component if *.svg?url
+        use: [
+          {
+            loader: "@svgr/webpack",
+            options: {
+              svgo: true,
+              svgoConfig: {
+                plugins: [
+                  {
+                    name: "preset-default",
+                    params: {
+                      overrides: {
+                        removeViewBox: false,
+                      },
+                    },
+                  },
+                ],
+              },
+            },
+          },
+        ],
+      },
+    );
+    return config;
+  },
 };
 
 export default nextConfig;
---
+++ svg.d.ts
@@ -0,0 +1,20 @@
+declare module "*.svg" {
+  import { FC, SVGProps } from "react";
+  const content: FC<SVGProps<SVGElement>>;
+  export default content;
+}
+
+declare module "*.svg?url" {
+  const content: any;
+  export default content;
+}
+
+declare module "*.webm" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.mp4" {
+  const src: string;
+  export default src;
+}
